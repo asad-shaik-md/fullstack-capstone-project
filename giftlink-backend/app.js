ec3d5512493095b22516171c2b42ef2101b08771
asad-shaik-md <shaik_mohammad2023@cms.ac.in>
2024-12-23 06:47:06 -0500

    All Done

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,6 +7,7 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
+
 const app = express();
 app.use("*",cors());
 const port = 3060;
@@ -15,31 +16,24 @@ const port = 3060;
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
-.catch((e) => console.error('Failed to connect to DB', e));
+    .catch((e) => console.error('Failed to connect to DB', e));
 
 
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftroutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
-
-
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('./api/gifts', giftroutes)
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('./api/search', searchRoutes)
-
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
